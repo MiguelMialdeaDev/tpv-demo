9168a1a757d35c3d801573136f4a555278aeb6b6
MiguelMialdeaDev <miguelamialdea@gmail.com>
2026-07-15 09:57:51 +0200

    TPV Kiosco - demo (build)

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Program%20Files/Git/tpv-demo/sw.js', { scope: '/Program%20Files/Git/tpv-demo/' })})}
